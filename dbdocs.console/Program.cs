495180262facec9e93aadd1ddd942e10f8ebb3e0
Mateusz Chrupczalski <m.chrupczalski@outlook.com>
2021-09-01 20:53:50 +0100

    Added class library and console projects

---
+++ dbdocs.console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dbdocs.console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
